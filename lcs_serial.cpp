c272c45873cb9b36be55fc6b35c8d4044ba0970e
Jesse <jessebot.git@gmail.com>
2024-12-01 17:58:28 -0800

    display correct length

--- lcs_serial.cpp
+++ lcs_serial.cpp
@@ -133,10 +133,11 @@ int main(int argc, char **argv) {
     // Print all found LCS sequences
     size_t nSubsequences = lcsResults.size();
     if (nSubsequences > 0) {
-        printf("LCS length : %zd\n", nSubsequences);
+        size_t lcsLength = lcsResults[0].size();
+        printf("LCS length : %zd\n", lcsLength);
 
         for (const string &lcs : lcsResults) {
-            printf("Length %zd subsequence : %s\n", nSubsequences, lcs.c_str());
+            printf("Length %zd subsequence : %s\n", lcsLength, lcs.c_str());
         }
     } else {
         printf("No subsequence found\n");
