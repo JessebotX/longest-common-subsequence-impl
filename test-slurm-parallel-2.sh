2796057b5ca58ba858886885a30bb6e9be3bff28
Jesse <jessebot.git@gmail.com>
2024-12-05 12:36:45 -0800

    add new test files

---
+++ test-slurm-parallel-2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=2
+#SBATCH --time=10:00
+#SBATCH --mem=5G
+#SBATCH --partition=slow
+
+# 10x10
+srun ./lcs_parallel -t 2 -x selvyjg0he -y lx3oeh8yso
+
+# 25x25
+srun ./lcs_parallel -t 2 -x qqz96admjoiekgvywb7n55qr0 -y u8dn8wkxiovwa382m77xp137m
+
+# 50x50
+srun ./lcs_parallel -t 2 \
+    -x 82t43c8ndm1lbsebl4jfuutw224s9ubzi78k65tidd7b2p9l10 \
+    -y ikdugd5uq3rox5lwz63adyvihu2cuo4dkvi1m7p9f7w1nk4za0 
